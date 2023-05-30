344b306b296324a2c95e94f90723b2ad53923ffa
Sofia Tula <2320454@ucc.edu.ar>
2023-05-30 09:41:49 -0300

    Se modifico

--- java.js
+++ java.js
@@ -1,7 +1,32 @@
 /**
- * Conversion de unidades, de metros, yardas, pies y pulgadas.
- * @method
- * @param
- * @param
- * @return
+ * Firma del cliente cuando realiza el pago por medio de tarjeta.
+ * Firmar
  */
+
+var bandera;
+function dibujar(event){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function (){bandera = true};
+    canvas.onmouseup = function (){bandera = false};
+
+    if(bandera){
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
+    }
+
+
+}
+
+function limpiarCanvas(){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx =  canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+}
