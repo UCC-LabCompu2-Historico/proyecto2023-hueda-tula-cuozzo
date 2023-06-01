b80db637e2c572b1a0f161d6bb1a6f8c0989219a
SofiaCuozzo <2313251@ucc.edu.ar>
2023-06-01 20:21:07 -0300

    Cambios

--- java.js
+++ java.js
@@ -5,13 +5,71 @@
  * @param
  * @return
  */
-function GuardarLSBP(){
-    let cant, tipo;
-    cant = document.getElementById(cantblack1).value;
-    tipo=document.getElementsById(miss_america).value;
-    localStorage.setItem("canthamb", cant);
-    localStorage.setItem("tipohamb", tipo);
+
+function calculo() {
+    var cant = parseInt(document.getElementById("cantblack1").value)||0;
+    var tipo=parseInt(document.getElementsByName("").value) || 0;
+    var iva = parseInt(document.getElementById("cantIva").value)||0;
+    var subt = cant*pUnit;
+    var cantIva = (subt*iva)/100;
+    var total=subt+cantIva;
+
+    document.getElementById("IVA").value= cantIva;
+    document.getElementById("Total").value=total;
 }
+
+// function GuardarLSBP(){
+//     let cant, tipo, i;
+//     cant = document.getElementById("cantblack1").value;
+//     tipo=document.getElementsByName("miss_america")[0].value;
+//     if(tipo=="simple"){
+//         tipo=1450;
+//     }
+//     else if(tipo=="doble"){
+//         tipo=1750;
+//     }
+//     else if(tipo=="triple"){
+//         tipo=2000;
+//     }
+//     localStorage.setItem("canthambLS", cant);
+//     localStorage.setItem("tipohambLS", tipo);
+//     window.open("formasdepago.html");
+// }
+// function CargarLS(){
+//     let cant1, tipo1;
+//     cant1=localStorage.getItem("canthambLS");
+//     tipo1=localStorage.getItem("tipohambLS");
+//     document.getElementById("total").value=Number(cant1)*Number(tipo1);
+//
+//     }
+function cargarWeb(){
+    var cant, tipo, urlComp;
+    cant=document.getElementById("cantblack1").value;
+    tipo=document.getElementsByName("miss_america")[0].value;
+    if(tipo=="simple"){
+        tipo=1450;
+    }
+    else if(tipo=="doble"){
+        tipo=1750;
+    }
+    else if(tipo=="triple"){
+        tipo=2000;
+    }
+    urlComp="formasdepago.html#" + cant + "#" + tipo;
+    console.log(urlComp)
+    window.open(urlComp);
+
+}
+function cargarResultado(){
+    var urlComp, can, tip;
+
+    urlComp=window.location.href.split("/")[5];
+    can=urlComp.split("#")[1];
+    tip=urlComp.split("#")[2];
+    document.getElementById("dist").value=can*tip;
+}
+
+
 
 var bandera;
 function dibujar(event){
@@ -32,11 +90,10 @@ function dibujar(event){
 
 
 }
-function limpiarCanvas(){
+function limpiarCanvas() {
     var canvas = document.getElementById("canvasAdibujar");
-    var ctx =  canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
     canvas.width = canvas.width;
-
 }
 
 
