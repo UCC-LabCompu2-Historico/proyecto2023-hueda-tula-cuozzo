1a8d77fc959b422eaa3c6d55deb1464a155e5d86
Sofia Tula <2320454@ucc.edu.ar>
2023-06-04 13:36:21 -0300

    Se realizaron cambios

--- java.js
+++ java.js
@@ -247,7 +247,7 @@ function dibujar(event){
     canvas.onmouseup = function (){bandera = false};
 
     if(bandera){
-        ctx.fillRect(posX - 12, posY - 587, 3, 3);
+        ctx.fillRect(posX, posY, 3, 3);
         ctx.fill;
     }
 
